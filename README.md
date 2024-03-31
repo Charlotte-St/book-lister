9bd4e6f60aa7261ea9dff91dd46fdf5ad49727f6
Lotte-Et <charlotte.stowe@gmail.com>
2024-03-30 20:04:03 -0400

    CS - added randomList to get length of list table

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Book Lister
 
-
 ## Resources
 
 Font Awesome
@@ -10,5 +9,7 @@ Font Awesome
 ### Tutorials 
 
 [CSV Parsing Tutorial](https://www.youtube.com/watch?v=9_x-UIVlxgo)
+
 [Bootstrap 5 Crash Course Tutorial #16 - Modals](https://www.youtube.com/watch?v=tt5uUMQgzl0)
+
 [Import CSV data into PostgreSQL using Node.js](https://www.bezkoder.com/node-js-csv-postgresql/)
--- public/js/randomList.js
+++ public/js/randomList.js
@@ -1,49 +1,20 @@
-const express = require('express');
-const { Pool } = require('pg');
-require('dotenv').config();
+var index;
 
+const getListLength = async () => {
 
-app.use(express.urlencoded({ extended: false}));
-app.use(express.json());
-
-//Connect to pool
-
-const pool = new Pool(
-   {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: 'localhost', 
-    database: process.env.DB_NAME
-   }
-)
-
-pool.connect();
-
-//Get count of lists
-
-var listCount;
-
-app.get('/api/book', (req, res) => {
-    const sql = 'SELECT COUNT(*) FROM book';
-
-    pool.query(sql, (err, res) => {
-        if (err) {
-            res.status(500).json(err);
-            return;
+     fetch('/api/list/', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        plain: true
         }
-        res.status(200).json();
-        console.log(res.json());
-        listCount = res.json();
-    })
-})
+    }).then((response) => response.json()).then(
+        (data) => {resultData = data;
+            console.log(resultData.length);
+            index = resultData.length
+                });
+};
 
+getListLength();
 
-//get random list indexes
-function getRandomIndex(data) {  
-
-    for (var i = 0; i < 10; i++){
-
-    var num = Math.floor(Math.random() * data);
-    cityCodes.push(num);
-    }
-}
+console.log(index);
