d0bcb6596456575b722acb466ea8f9223c1fa36c
Lotte-Et <charlotte.stowe@gmail.com>
2024-04-03 22:37:46 -0400

    CS - Removed unnecessary console logs; updated README

--- public/js/deleteBook.js
+++ public/js/deleteBook.js
@@ -1,5 +1,3 @@
-console.log('Running Delete Book')
-
 const delBookHandler = async (event) => {
     if (event.target.hasAttribute('data-id')) {
       const id = event.target.getAttribute('data-id');
